34966b28c66d6d5eebd0cbc8e61b4ee3903651cc
Dxos2001 <j.alexander2001@hotmail.com>
2025-07-03 01:20:07 -0500

    Add Firebase hosting configuration and update environment settings

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pelicula/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const response = await fetch('http://34.71.55.81:5208/Cartelera/PeliculasXSala');
+      const peliculas = await response.json();
+      // Extrae solo los IDs de cada película
+      return peliculas.map((pelicula: any) => ({ id: String(pelicula.id) }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
