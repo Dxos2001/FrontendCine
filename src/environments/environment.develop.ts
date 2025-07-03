9fb0fc8911ba77b3751cd50f1c6c6a66427f4e8b
Dxos2001 <j.alexander2001@hotmail.com>
2025-07-03 15:24:59 -0500

    Update API URLs to use the new domain for development and production environments

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://34.71.55.81:5208'
+  apiUrl: 'https://apicinenet.d-xos.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://34.71.55.81:5208'
+  apiUrl: 'https://apicinenet.d-xos.com'
 };
