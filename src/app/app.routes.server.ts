4cc32cec2c64c8f15f17bfdfe3e4787c648f6b91
Dxos2001 <j.alexander2001@hotmail.com>
2025-07-08 00:44:33 -0500

    Java 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,7 +6,7 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender,
     async getPrerenderParams() {
       //const response = await fetch('https://apicinenet.d-xos.com/Cartelera/PeliculasXSala');
-      const response = await fetch('https://backendcinejava.rj.r.appspot.com/Cartelera/PeliculasXSala');
+      const response = await fetch('https://backendcinejava.rj.r.appspot.com/cartelera/PeliculasXSala');
       const peliculas = await response.json();
       // Extrae solo los IDs de cada película
       return peliculas.map((pelicula: any) => ({ id: String(pelicula.id) }));
